03f426fbf2d4124d7a571579e0389c836572f851
VidhullaSaravana <90876747+VidhullaSaravana@users.noreply.github.com>
2023-03-12 17:05:32 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,54 @@
+import random
+response = "y"
+while response == "y":
+    no = random.randint(1, 6)
+
+
+if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+response = input("y to roll and n to exit")    
+
+if no == 2:
+    print("[-----]")
+    print("[     ]")
+    print("[  00 ]")
+    print("[     ]")
+    print("[-----]")
+response = input("y to roll and n to exit")    
+
+if no == 3:
+    print("[-----]")
+    print("[     ]")
+    print("[ 000 ]")
+    print("[     ]")
+    print("[-----]")
+response = input("y to roll and n to exit")    
+
+if no == 4:
+    print("[-----]")
+    print("[0   0]")
+    print("[     ]")
+    print("[0   0]")
+    print("[-----]")
+response = input("y to roll and n to exit")    
+
+if no == 5:
+    print("[-----]")
+    print("[0   0]")
+    print("[  0  ]")
+    print("[0   0]")
+    print("[-----]")
+response = input("y to roll and n to exit")    
+
+if no == 6:
+    print("[-----]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[0   0]")
+    print("[-----]")
+
+response = input("y to roll and n to exit")    
